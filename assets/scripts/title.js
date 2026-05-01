c40fadf9c15426164be1a3e68e6e5f0fd4af6503
leo <l.scarin@kabk.nl>
2026-05-01 18:23:38 +0200

    title udpate

--- assets/scripts/title.js
+++ assets/scripts/title.js
@@ -1,7 +1,7 @@
 const title = document.getElementById('title');
-const titles = ['an artist', 'a designer', 'a programmer', 'an educator', 'a media artist', 'doing his best', 'an interaction designer', 'a handsome young man', 'a geek', 'a code crafter', 'a nerd', 'a coder', 'a permacomputer', 'a critical thinker', 'a creative technologist', 'in love', 'sometimes tired', 'a good friend']
-const emoji = ['🛹', '🌱 ', '💻', '🪰', '🍌', '📍', '🌞', '🐢', '🤌']
+const titles = ['an artist', 'a designer', 'a programmer', 'an educator', 'a media artist', 'doing his best', 'an interaction designer', 'a handsome young man', 'a geek', 'a code crafter', 'a nerd', 'a coder', 'a permacomputer', 'a critical thinker', 'a creative technologist', 'in love', 'sometimes tired', 'a good friend'];
+const emoji = ['🛹', '🌱 ', '💻', '🪰', '🍌', '📍', '🌞', '🐢', '🤌'];
 
 setInterval(() => {
-    title.innerHTML = `${emoji[Math.floor(Math.random() * emoji.length)]} Leo Scarin is ${titles[Math.floor(Math.random() * titles.length)]}`
+    title.innerHTML = `${emoji[Math.floor(Math.random() * emoji.length)]} Leo Scarin is ${titles[Math.floor(Math.random() * titles.length)]}`;
 }, 1000);
